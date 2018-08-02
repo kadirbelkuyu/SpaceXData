82f2124c7d1fcb5b132a63e9f705b98936730f09
Seda Yıldız <sedanuryildiz23@gmail.com>
2018-08-02 15:05:58 +0300

    menu while true added

--- Data.py
+++ Data.py
@@ -48,28 +48,27 @@ class SpaceX():
     launch_success = None
 
     def __init__(self):
-        self.rocket_name = None
-        self.launch_year = None
-        self.launch_success = None
-        print("Welcome")
-        menu = input("""Please select your operation. 
-              1-Search by the rocket name 
-              2- Search by the launch year 
-              3-Search by the launch success              
-              """)
-        if menu == "1":
-            rocket_name = input("Please write the rocket name you want to search")
-            self.search(rocket_name)
-        if menu == "2":
-            launch_year = input("Please write the year you want to search")
-            self.search(launch_year)
-        if menu == "3":
-            success_type = input("""
-            1- Show rockets with launch success
-            2- Show rockets with land success
-            3- Show rockets with reuse success
-            """)
-            self.rocket_success(success_type)
+
+        while True:
+            print("Welcome")
+            menu = input("""Please select your operation. 
+                  1-Search by the rocket name 
+                  2- Search by the launch year 
+                  3-Search by the launch success              
+                  """)
+            if menu == "1":
+                rocket_name = input("Please write the rocket name you want to search")
+                self.search(rocket_name)
+            if menu == "2":
+                launch_year = input("Please write the year you want to search")
+                self.search(launch_year)
+            if menu == "3":
+                success_type = input("""
+                1- Show rockets with launch success
+                2- Show rockets with land success
+                3- Show rockets with reuse success
+                """)
+                self.rocket_success(success_type)
 
     def search(self,keyword):
         pass
